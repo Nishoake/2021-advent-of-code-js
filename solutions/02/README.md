470ce90d74e24979d63c84243a2bcf3d2aadb4aa
Nishoake <nsribava@uwaterloo.ca>
2021-12-02 08:39:12 -0500

    Completed Part One of Day 2: Dive! 🤿

---
+++ solutions/02/README.md
@@ -0,0 +1,3 @@
+# Day 02: Dive! 🤿
+
+## Link: <https://adventofcode.com/2021/day/2>
---
+++ solutions/02/index.js
@@ -0,0 +1,51 @@
+const Input = require('./input')
+// const Input = require('./input_sample')
+
+// Parsing the input as an Array of numbers
+const getInput = Input.commands.split('\n').map(String)
+
+
+// Defined function for part1 of the problem
+const part1 = (input) => {
+  let depth = 0
+  let breadth = 0
+  
+  for (let i = 0; i < input.length; i++)  {
+    const parsed = input[i].match(/(\w*)\s(\d*)/)
+    const direction = parsed[1]
+    const value = parseInt(parsed[2])
+    
+    switch (direction) {
+      case 'up':
+        depth -= value
+        break;
+      case 'down':
+        depth += value
+        break;
+      case 'forward':
+        breadth += value
+        break;
+    }
+  }
+
+  return depth * breadth
+}
+
+// Execute function for part1 of the problem
+const result1 = part1(getInput)
+console.log(`Part 1: ... = ${result1}`)
+
+
+// // Defined function for part2 of the problem
+// const part2 = (input) => {
+//   console.log(`The parsed input = ${input}`)
+// }
+
+// // Execute function for part1 of the problem
+// const result2 = part2(getInput)
+// console.log(`Part 2: ... = ${result2}`)
+
+// export default {
+//   part1,
+//   part2,
+// }
