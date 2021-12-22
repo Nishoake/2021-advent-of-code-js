a5967a31d4efbed2d6078bfe72b151abeddd9934
Nishoake <nsribava@uwaterloo.ca>
2021-12-21 23:36:34 -0500

    Completed Part Two of Day 3: Binary Diagnostic✌🏽✌🏾

--- solutions/03/index.js
+++ solutions/03/index.js
@@ -4,15 +4,12 @@ const Input = require('./input')
 // Parsing the input as an Array of numbers
 const getInput = Input.diagnostics.split('\n').map(String)
 
+const findBitFrequency = (input) => {
+  const counterObject = {}
 
-// Defined function for part1 of the problem
-const part1 = (input) => {
-  let counterObject = {}
-  let gammaRate = ''
-  let espilonRate = ''
-
+  // Iterate over the input array to find frequency of the '1' bit
   for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input[i].length; j++)  {
+    for (let j = 0; j < input[i].length; j++) {
       // Evaluates to true if integer is 1
       if (parseInt(input[i][j])) {
         // We use an integer as an object key
@@ -21,17 +18,51 @@ const part1 = (input) => {
       }
     }
   }
+  return counterObject
+}
 
-  for (const column in counterObject ) {
-    counterObject[column] > input.length / 2 ? gammaRate += 1 : gammaRate += 0
-    counterObject[column] > input.length / 2 ? espilonRate += 0 : espilonRate += 1
+const findMostCommonBit = (input, index) => {
+  let counter = 0
+
+  // Iterate over the input array to find frequency of the '1' bit
+  for (let i = 0; i < input.length; i++) {
+    if (parseInt(input[i][index])) counter++
+  }
+  
+  const mostCommon = (counter >= input.length / 2) ? "1" : "0"
+  return mostCommon
+}
+
+const findLeastCommonBit = (input, index) => {
+  let counter = 0
+
+  // Iterate over the input array to find frequency of the '1' bit
+  for (let i = 0; i < input.length; i++) {
+    if (parseInt(input[i][index])) counter++
   }
 
-  
-  console.log(`Gamma Rate = ${gammaRate}`)
-  console.log(`Espilon Rate = ${espilonRate}`)
+  const leastCommon = (counter >= input.length / 2) ? "0" : "1"
+  return leastCommon
+}
 
-  return parseInt(gammaRate, 2) * parseInt(espilonRate, 2)
+const getGammaRate = (counterObject, input) => {
+  let gammaRate = ''
+  for (const column in counterObject) counterObject[column] >= input.length / 2 ? gammaRate += 1 : gammaRate += 0
+  return gammaRate
+}
+
+const getEspilonRate = (counterObject, input) => {
+  let espilonRate = ''
+  for (const column in counterObject) counterObject[column] > input.length / 2 ? espilonRate += 0 : espilonRate += 1
+  return espilonRate
+}
+
+// Defined function for part1 of the problem
+const part1 = (input) => {
+  const counterObject = findBitFrequency(input)
+  const SubmarinePowerConsumption = parseInt(getGammaRate(counterObject, input), 2) * parseInt(getEspilonRate(counterObject, input), 2)
+
+  return SubmarinePowerConsumption
 }
 
 // Execute function for part1 of the problem
@@ -39,19 +70,41 @@ const result1 = part1(getInput)
 console.log(`Part 1: ... = ${result1}`)
 
 
-// // Defined function for part2 of the problem
-// const part2 = (input) => {
-//   console.log(`The parsed input = ${input}`)
-  // Use the Gamma Rate value for oxygen generator rating in part two
-  // Use the Espilon Rate value for CO2 scrubber rating in part two
-  // Remove value from an array at nth place?
-  // if (input.length === 1) break
-  // Try a recursive approach
-// }
+// Defined function for part2 of the problem
+const part2 = (input) => {
 
-// // Execute function for part1 of the problem
-// const result2 = part2(getInput)
-// console.log(`Part 2: ... = ${result2}`)
+  // Function to find the single matching number for oxygenGeneratorRating
+  const getOGR = (input, columnNumber) => {
+    if (input.length === 1 ) return input[0]
+
+    const target = findMostCommonBit(input, columnNumber)
+
+    const subArray = input.filter(bit => bit[columnNumber] === target)
+
+    return getOGR(subArray, columnNumber + 1)
+  }
+
+  // Function to find the single matching number for co2ScrubberRating
+  const getCSR = (input, columnNumber) => {
+    if (input.length === 1 ) return input[0]
+
+    const target = findLeastCommonBit(input, columnNumber)
+
+    const subArray = input.filter(bit => bit[columnNumber] === target)
+
+    return getCSR(subArray, columnNumber + 1)
+  }
+  
+  const oxygenGeneratorRating = getOGR(input, 0)
+  const co2ScrubberRating = getCSR(input, 0)
+  const lifeSupportRating = parseInt(oxygenGeneratorRating, 2) * parseInt(co2ScrubberRating, 2)
+
+  return lifeSupportRating
+}
+
+// Execute function for part1 of the problem
+const result2 = part2(getInput)
+console.log(`Part 2: ... = ${result2}`)
 
 // export default {
 //   part1,
