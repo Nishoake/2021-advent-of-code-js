b7b73ba5efd173f438186d340190cd80ef2d3718
Nishoake <nsribava@uwaterloo.ca>
2022-02-20 12:39:51 -0500

    Completed Part One of Day 4: Giant Squid 🦑

---
+++ solutions/04/index.js
@@ -0,0 +1,74 @@
+// const Input = require('./input')
+const Input = require('./input')
+
+// Parsing the input as an Array of numbers
+const getInput = Input.bingo.split('\n\n').map(String)
+
+// Defined function for part1 of the problem
+const part1 = (input) => {
+
+  // Parsing the cards
+  const parsedInput = input.map(line => line
+      .replace(/[\n ,]+/g, " ")
+      .trim()
+      .split(" ")
+      .map(numString => parseInt(numString))
+  )
+
+  // Destructuring parsed data
+  const [numbersCalled, ...bingoBoards] = parsedInput
+
+  // Logic for determining bingo
+  for (let i = 0; i < numbersCalled.length; i++)  {
+    for (let j = 0; j < bingoBoards.length; j++) {
+      // Check if number exists in bingo board
+      const numCheck = bingoBoards[j].indexOf(numbersCalled[i])
+
+      if (numCheck !== -1)  {
+        // toggle to 0
+        bingoBoards[j][numCheck] = 0
+        // Determine the row and colum
+        const row = Math.floor(numCheck / 5)
+        const column = numCheck % 5
+
+        // Check for bingo
+        const rowCheck = (bingoBoards[j][row * 5 + 0] === 0 && bingoBoards[j][row * 5 + 1] === 0 && bingoBoards[j][row * 5 + 2] === 0 && bingoBoards[j][row * 5 + 3] === 0 && bingoBoards[j][row * 5 + 4] === 0)
+        const columnCheck = (bingoBoards[j][column] === 0 && bingoBoards[j][column + 5] === 0 && bingoBoards[j][column + 10] === 0 && bingoBoards[j][column + 15] === 0 && bingoBoards[j][column + 20] === 0)
+        const bingoCheck = rowCheck || columnCheck
+
+        if (bingoCheck) {
+          // reducer function
+          function getSum(total, num) {
+            return total + num
+          }
+          // Calculate and return result
+          const remainderSum = bingoBoards[j].reduce(getSum, 0)
+          const result = remainderSum * numbersCalled[i]
+          return result
+        }
+      }
+    }
+  }
+
+  console.log(`No Bingo`)
+
+}
+
+// Execute function for part1 of the problem
+const result1 = part1(getInput)
+console.log(`Part 1: ... = ${result1}`)
+
+
+// // Defined function for part2 of the problem
+// const part2 = (input) => {
+//   console.log(`The parsed input = ${input}`)
+// }
+
+// // Execute function for part1 of the problem
+// const result2 = part2(getInput)
+// console.log(`Part 2: ... = ${result2}`)
+
+// export default {
+//   part1,
+//   part2,
+// }
